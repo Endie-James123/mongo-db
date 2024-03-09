f3f5b38338366d96bd815dd0f4222981d3c24279
Endie-James123 <ndopnnoabasi@gmail.com>
2024-03-09 11:37:04 +0100

    added a route to get all the automobiles

--- src/automobile/automobile.controller.ts
+++ src/automobile/automobile.controller.ts
@@ -1,12 +1,20 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AutomobileService } from './automobile.service';
 import { AutoDto } from 'src/dto/auto.dto';
 
 @Controller('automobile')
 export class AutomobileController {
     constructor(private readonly AutoService:AutomobileService) {}
+
+    //Route to add a new automobile
     @Post('add')
   async createProduct(@Body() payload: AutoDto) {
     return await this.AutoService.addAuto(payload);
+  }
+
+  //Route to get all the automobiles
+  @Get()
+  findAllAutomobiles(){
+    return this.AutoService.getAllAuto();
   }
 }
--- src/automobile/automobile.service.ts
+++ src/automobile/automobile.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { AutoDto } from 'src/dto/auto.dto';
@@ -7,8 +7,20 @@ import { Auto } from 'src/schema/auto.shema';
 @Injectable()
 export class AutomobileService {
   constructor(@InjectModel(Auto.name) private authModel: Model<Auto>) {}
+
+  //Method to add auto
   async addAuto(payload: AutoDto){
     const saveAuto = new this.authModel(payload);
     return await saveAuto.save();
+  }
+
+  //Method to get all auto
+  async getAllAuto(){
+    try{
+      const findAll = await this.authModel.find();
+      return findAll;
+    }catch(theError){
+      throw new NotFoundException('Could not find all autos');
+    }
   }
 }
