6d3841b6f21cfb8cd0a8a75f6c50916a7b488eb6
Endie-James123 <ndopnnoabasi@gmail.com>
2024-04-15 11:19:57 +0100

    Adding descriptive comments

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { SignupDto } from './dto/signup.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Signup } from './Schemas/auth.schema';
@@ -7,46 +11,54 @@ import { JwtService } from '@nestjs/jwt';
 import { loginDto } from './dto/login.dto';
 import * as bcrypt from 'bcrypt';
 import { UpdateDto } from './dto/Update.dto';
- 
+
 @Injectable()
 export class AuthService {
   private readonly blockedUsers: Signup[] = [];
-  constructor(@InjectModel("Signup") private SignupModel: Model<Signup>,
-  private jwtService: JwtService) {}
- 
-  //SIGN-UP(Registering a new user)  
+  constructor(
+    @InjectModel('Signup') private SignupModel: Model<Signup>,
+    private jwtService: JwtService,
+  ) {}
+
+  //SIGN-UP(Registering a new user)
   //async: This keyword indicates that the function is asynchronous, meaning it can perform operations that may take some time to complete, such as accessing a database or making HTTP requests.
-  async RegisterUser(payload:SignupDto){
+  async RegisterUser(payload: SignupDto) {
     //Destructuring the payload
-    const {password, email, age, ...rest} = payload//The ...rest syntax gathers any remaining properties into a new object called rest
-    const hashedPassword = await bcrypt.hash(password, 10)
-    const findEmail = await this.SignupModel.findOne({email})
+    const { password, email, age, ...rest } = payload; //The ...rest syntax gathers any remaining properties into a new object called rest
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const findEmail = await this.SignupModel.findOne({ email });
     //the code that checks if the user is up to 18yrs
-    if (age < 18){
-      //the code below pushes the user to the blocked-users array 
+    if (age < 18) {
+      //the code below pushes the user to the blocked-users array
       this.blockedUsers.push(payload);
       //the error to throw to the user
-      throw new UnauthorizedException("You must be atleast 18 years old ")
+      throw new UnauthorizedException('You must be atleast 18 years old ');
     }
-    
+
     //the code belows checks if the user's email has already been registered in the database
-    if(findEmail){
+    if (findEmail) {
       //Error to throw if the email already exists in the database
-      throw new UnauthorizedException("Email already exists")
+      throw new UnauthorizedException('Email already exists');
     }
     //the code below checks if the user's email has been blocked
-    const findEmailInBlockedUsers = this.blockedUsers.find(user => user.email === email);
-  if (findEmailInBlockedUsers) {
-    //if found in the BlockedUsers array, deny access and throw reason for access denial
-    throw new UnauthorizedException("This Email is blocked due to underage, trying signing up with another email");
+    const findEmailInBlockedUsers = this.blockedUsers.find(
+      (user) => user.email === email,
+    );
+    if (findEmailInBlockedUsers) {
+      //if found in the BlockedUsers array, deny access and throw reason for access denial
+      throw new UnauthorizedException(
+        'This Email is blocked due to underage, trying signing up with another email',
+      );
+    }
+    //if user has passed all the required permissions and elligible go ahead and register the user
+    const Register = new this.SignupModel({
+      password: hashedPassword,
+      email,
+      ...rest,
+    }); //this codee hashes the users password
+    const Registered = Register.save();
+    return Registered;
   }
-  //if user has passed all the required permissions and elligible go ahead and register the user 
-    const Register = new this.SignupModel({password:hashedPassword, email, ...rest})//this codee hashes the users password
-    const Registered = Register.save()
-    return Registered
-  }
-
-
 
   //Sign In (logging in an already existing user)
   async signIn(payload: loginDto) {
@@ -59,15 +71,15 @@ export class AuthService {
     // Check if user doesn't exists
     if (!findUser) {
       //error to throw if user doesn't exist
-        throw new UnauthorizedException('User not found');
+      throw new UnauthorizedException('User not found');
     }
 
     // Checking if the provided password matches the hashed password in the database
     const passwordMatch = await bcrypt.compare(password, findUser.password);
 
     if (!passwordMatch) {
-        // Error to throw if the password does not match the password stored in the database
-        throw new UnauthorizedException('Invalid password');
+      // Error to throw if the password does not match the password stored in the database
+      throw new UnauthorizedException('Invalid password');
     }
 
     // Creating a variable to hold the found user
@@ -78,12 +90,12 @@ export class AuthService {
 
     // Returning the access token and a message
     return {
-        message: `${findUser.name} is logged in successfully`,
-        access_token: access_token,
+      message: `${findUser.name} is logged in successfully`,
+      access_token: access_token,
     };
-}
+  }
 
-  async getAllUsers(){ 
+  async getAllUsers() {
     try {
       const findAll = await this.SignupModel.find();
       return findAll;
@@ -93,11 +105,11 @@ export class AuthService {
   }
 
   //Logic to get all Blocked users from the BlockedUsers array
-  async getAllBlockedUsers(){
-    try{
+  async getAllBlockedUsers() {
+    try {
       const findAll = this.blockedUsers;
       return findAll;
-    }catch (theError){
+    } catch (theError) {
       throw new NotFoundException('Could not get all BlockedUsers');
     }
   }
@@ -110,36 +122,35 @@ export class AuthService {
     return { status: 200, message: `${name} was deleted successfully` };
   }
 
-  async getOne(name:string){
-      const findOne = await this.SignupModel.findOne({name:name});
-      if(findOne){
+  async getOne(name: string) {
+    const findOne = await this.SignupModel.findOne({ name: name });
+    if (findOne) {
       return findOne;
-      }
+    }
+    throw new NotFoundException(`${name} not found`);
+  }
+
+  //Logic to Update user by name, hashed the new password if changed, if users age less than 18 upon Update throw error
+  async UpdateUserByName(name: string, payload: Partial<UpdateDto>) {
+    const findOne = await this.SignupModel.findOne({ name: name });
+    if (!findOne) {
       throw new NotFoundException(`${name} not found`);
     }
-
-
-    //Logic to Update user by name, hashed the new password if changed, if users age less than 18 upon Update throw error
-    async UpdateUserByName(name:string, payload: Partial<UpdateDto>){
-      const findOne = await this.SignupModel.findOne({name:name});
-      if(!findOne){ 
-        throw new NotFoundException(`${name} not found`);
-      } 
-      Object.keys(payload).forEach((key) => {
-        findOne[key] = payload[key];
-      });
-      if(payload.password){
-        const hashedPassword = await bcrypt.hash(payload.password, 10)
-        payload.password = hashedPassword
-      }
-      if (payload.age < 18){
-        throw new UnauthorizedException("User must be atleast 18 years old ")
-      }
-      Object.assign(findOne, payload);
-      const updated = await findOne.save();
-      return {
-        message: 'User updated successfully',
-        NewData: updated,
-      }
+    Object.keys(payload).forEach((key) => {
+      findOne[key] = payload[key];
+    });
+    if (payload.password) {
+      const hashedPassword = await bcrypt.hash(payload.password, 10);
+      payload.password = hashedPassword;
+    }
+    if (payload.age < 18) {
+      throw new UnauthorizedException('User must be atleast 18 years old ');
+    }
+    Object.assign(findOne, payload);
+    const updated = await findOne.save();
+    return {
+      message: 'User updated successfully',
+      NewData: updated,
+    };
   }
 }
